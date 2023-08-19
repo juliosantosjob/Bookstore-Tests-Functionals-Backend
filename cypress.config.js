9c75ed5ac6efdfa7aea2d1b6294098dc3eb111a9
Julio Santos <julio958214@gmail.com>
2023-08-19 14:04:13 -0300

    Code Refactoring

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,16 @@
 const { defineConfig } = require('cypress');
+require('dotenv').config();
 
 module.exports = defineConfig({
     e2e: {
         setupNodeEvents(on, config) {
-
+            config.baseUrl = process.env.BASE_URL;
             require('cypress-mochawesome-reporter/plugin')(on);
             return config;
         },
-        baseUrl: 'https://demoqa.com/',
         responseTimeout: 3000,
         reporter: 'cypress-mochawesome-reporter',
         video: false,
         screenshotOnRunFailure: false
     }
-}); 
+});
