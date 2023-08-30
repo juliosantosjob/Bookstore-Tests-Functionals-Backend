f58efca395f634ebc994e60ffef63956c40011d2
Julio <julio958214@gmail.com>
2023-08-30 18:32:48 -0300

    Code refactory

--- cypress.config.js
+++ cypress.config.js
@@ -8,11 +8,10 @@ module.exports = defineConfig({
             config.env.NAME = process.env.NAME;
             config.env.PASSWORD = process.env.PASSWORD;
             config.env.USER_ID = process.env.USER_ID,
-    
+
             require('cypress-mochawesome-reporter/plugin')(on);
             return config;
         },
-        responseTimeout: 4000,
         reporter: 'cypress-mochawesome-reporter',
         video: false,
         screenshotOnRunFailure: false
