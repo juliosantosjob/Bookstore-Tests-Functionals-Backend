541bfd5049b434e35771066983208d1a97de192d
Julio Santos <julioc.9730@gmail.com>
2024-03-10 17:31:48 -0300

    Update login.js

--- cypress/payloads/login.js
+++ cypress/payloads/login.js
@@ -6,7 +6,6 @@ module.exports = {
         userName: NAME,
         password: PASSWORD
     },
-
     dynamicData: {
         userName: faker.internet.userName().substring(5),
         password: faker.internet.password().substring(7) + 'M@123',
