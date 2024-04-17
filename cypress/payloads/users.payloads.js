4ad4a258f84e5ae2cc3d9dd834fe6a0b6318a958
Julio Santos <julioc.9730@gmail.com>
2024-04-17 20:22:51 -0300

    Update users.payloads.js

--- cypress/payloads/users.payloads.js
+++ cypress/payloads/users.payloads.js
@@ -2,11 +2,11 @@ import { faker } from '@faker-js/faker';
 const { NAME, PASSWORD } = Cypress.env();
 
 export default {
-    authUser_: {
+    _authUser: {
         userName: NAME,
         password: PASSWORD
     },
-    dynamicUser_: {
+    _dynamicUser: {
         userName: faker.internet.userName().substring(5),
         password: faker.internet.password().substring(7) + 'M@123'
     }
