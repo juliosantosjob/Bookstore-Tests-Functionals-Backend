8e9a277b18052a70839f4a64ba8bbaea39e2ffd7
Julio Cesar <juliosantosjob>
2024-04-25 15:23:00 -0300

    Ajustando erro das varivaeis de ambiente no github actions

--- cypress.config.js
+++ cypress.config.js
@@ -8,7 +8,14 @@ module.exports = defineConfig({
         video: false,
         screenshotOnRunFailure: false,
         setupNodeEvents(on, config) {
-            config.baseUrl = getBaseUrl(config);
+            const version = config.env.version;
+            const urls = {
+                local: process.env.LOCAL,
+                staging: process.env.STAGING,
+                prod: process.env.PROD
+            };
+
+            config.baseUrl = urls[version];
             config.env.NAME = process.env.NAME;
             config.env.PASSWORD = process.env.PASSWORD;
             config.env.USER_ID = process.env.USER_ID;
