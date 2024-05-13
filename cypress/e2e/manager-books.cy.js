48fb502c672631331151d0d453ce194485e8c994
Julio Cesar <juliosantosjob>
2024-05-13 13:46:42 -0300

    removendo flak tests do cenario para adicionar livro aos favoritos

--- cypress/e2e/manager-books.cy.js
+++ cypress/e2e/manager-books.cy.js
@@ -6,7 +6,7 @@ import { authUser } from '../payloads/users.payloads';
 const rand = Math.floor(Math.random() * books.length);
 
 describe('Manage books', () => {
-    let token, userId, isbn;
+    let token, userId, isbn_;
 
     it('Check information of a book', () => {
         cy.fixture('listBooks').then((list) => {
@@ -19,33 +19,43 @@ describe('Manage books', () => {
         });
     });
 
-    it('Add and remove a book from the favorites list', () => {
+    before(() => {
+        cy.loginUser(authUser)
+            .its('body.token')
+            .then(resp => token = resp);
+
         userId = Cypress.env('USER_ID');
+    });
 
-        cy.loginUser(authUser).then(({ body }) => {
-            token = body.token;
-
-            cy.getBookList().then(({ body }) => {
-                isbn = body.books[rand].isbn;
-
-                cy.addBooksFavorites(token, userId, isbn).then(({ status, body }) => {
-                    expect(status).to.equal(201);
-                    expect(body.books[0].isbn).to.equal(isbn);
+    Cypress._.times(100, () => {
+        it('Add and remove a book from the favorites list', () => {
+            cy.getBookList()
+                .its(`body.books[${rand}].isbn`)
+                .then((isbn) => {
+                    isbn_ = isbn;
     
-                    cy.removeBooks(token, userId).then(() => {
-                        cy.getProfile(token, userId).then(({ body }) => {
-                            expect(body.books).to.be.empty;
+                    cy.addBooksFavorites(
+                        token,
+                        userId,
+                        isbn_
+                    ).then(({ status, body }) => {
+                        expect(status).to.equal(201);
+                        expect(body.books[0].isbn).to.equal(isbn_);
+    
+                        cy.removeBooks(token, userId).then(() => {
+                            cy.getProfile(token, userId).then(({ body }) => {
+                                expect(body.books).to.be.empty;
+                            });
                         });
                     });
                 });
-            });
         });
     });
 
     it('Don\'t add a book that doesn\'t exist', () => {
-        isbn = 'invalid_isbn';
+        isbn_ = 'invalid_isbn';
 
-        cy.addBooksFavorites(token, userId, isbn).then(({ status, body }) => {
+        cy.addBooksFavorites(token, userId, isbn_).then(({ status, body }) => {
             expect(status).to.equal(400);
             expect(body.message).to.equal('ISBN supplied is not available in Books Collection!');
         });
@@ -54,7 +64,7 @@ describe('Manage books', () => {
     it('Do not add a book to the favorites list without authorization', () => {
         token = 'invalid_token';
 
-        cy.addBooksFavorites(token, userId, isbn).then(({ status, body }) => {
+        cy.addBooksFavorites(token, userId, isbn_).then(({ status, body }) => {
             expect(status).to.equal(401);
             expect(body.message).to.equal('User not authorized!');
         });
@@ -63,7 +73,7 @@ describe('Manage books', () => {
     it('Didn\'t add a book to the favorites list with an incorrect user ID.', () => {
         userId = 'invalid_user_id';
 
-        cy.addBooksFavorites(token, userId, isbn).then(({ status, body }) => {
+        cy.addBooksFavorites(token, userId, isbn_).then(({ status, body }) => {
             expect(status).to.equal(401);
             expect(body.message).to.equal('User Id not correct!');
         });
